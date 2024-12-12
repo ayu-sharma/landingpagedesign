3412eb6d8c0faf4f1c5a7fcbff1c0f3a5b082ad4
ayu-sharma <contactayusharma@gmail.com>
2024-12-12 17:40:30 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import drills from "../public/Images/drills.jpg";
-import weigths from "../public/Images/weights.jpg";
+import weights from "../public/Images/weights.jpg";
 import yoga from "../public/Images/yoga.jpg";
+import Image from "next/image";
 export default function Home() {
   const items = [
     {
       id: 1,
       title: "STRENGTH",
-      image: weigths,
+      image: weights,
     },
     {
       id: 2,
@@ -22,20 +22,10 @@ export default function Home() {
       image: drills,
     },
   ];
-
-  const [isExpanded, setIsExpanded] = useState(2);
-
-  console.log("isExpanded", isExpanded);
-  const handleClick = (id) => {
-    console.log("clicked", id);
-    if (isExpanded) {
-      setIsExpanded(id);
-    }
-  };
   return (
     <>
-      <div>
-        <div className="flex items-center justify-center h-[100vh]">
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex relative h-[50vh] px-auto max-w-6xl overflow-hidden mx-auto w-full">
           {items.map((item, index) => {
             const roundedClass =
               index === 0
@@ -43,29 +33,31 @@ export default function Home() {
                 : index === items.length - 1
                 ? "rounded-r-xl"
                 : "";
+
+                const middleClass =
+                index === 1 ? "flex-[3]": "flex-[1]";
             return (
               <div
                 key={item.id}
-                className={`aspect-auto ${roundedClass} relative`}
+                className={`relative ${middleClass} hover:flex-[3] ease-in-out duration-500`}
               >
                 <Image
                   onClick={() => handleClick(item.id)}
                   src={item.image}
                   alt={item.title}
-                  className={`${roundedClass} ${
-                    isExpanded === item.id ? "" : ""
-                  }`}
+                  className={`absolute w-full object-cover h-full ${roundedClass}`}
                   width={350}
                   height={300}
                 />
                 <div className="absolute top-1 left-3 text-white">
                   {item.title}
                 </div>
-              </div>
+                </div>
             );
           })}
         </div>
       </div>
+      {/* <HandleImage/> */}
     </>
   );
 }
